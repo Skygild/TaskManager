f7b4416c52113c3183aa9f2de7f27c9d41f161b7
Lev Skyler Roan <skylevroan@gmail.com>
2024-03-23 16:37:58 +0800

    Created create account function

--- server/src/api/register.ts
+++ server/src/api/register.ts
@@ -1,24 +1,27 @@
 import mysql, { Pool } from "mysql2";
 import { Database } from "../db/dbConnection";
 import { mysqlConfig } from "../index";
+import bcrypt from "bcrypt";
+import { Request, Response } from "express";
 
 export class Register {
   private db: Database;
   private pool: Pool;
-
   constructor() {
     this.db = new Database(mysqlConfig);
     this.pool = this.db.getPool();
   }
 
-  async createAccount(email: string, username: string, password: any) {
+  async createAccount(req: Request, res: Response) {
     this.pool.getConnection(async (err, connection) => {
       try {
         if (err) {
           console.log(err);
           return;
         }
-        connection.query("INSERT INTO users (email, username, password) VALUES (?, ?, ?)", [email, username, password], (err, result) => {
+        const { email, username, password }: any = req.body;
+        const hashedPassword = await bcrypt.hash(password, 10);
+        connection.query("INSERT INTO users (email, username, password) VALUES (?, ?, ?)", [email, username, hashedPassword], (err, result) => {
           if (err) {
             console.log(err);
             return;
