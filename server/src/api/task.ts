2d3594049d449c6d7b04623380af059bb70bde6b
Lev Skyler Roan <skylevroan@gmail.com>
2024-04-09 13:31:42 +0800

    Create task func

--- server/src/api/task.ts
+++ server/src/api/task.ts
@@ -1,22 +1,29 @@
-// if (connection) {
-//     const { taskTitle, taskDesc, createdAt, deadline, createdBy, completed } = req.body;
-//     connection.query("INSERT INTO task (taskTitle, taskDesc, createdAt, deadline, createdBy, completed) VALUES (?, ?, ?, ?, ?, ?)", [taskTitle, taskDesc, createdAt, deadline, createdBy, completed], (err, result) => {
-//       if (err) {
-//         console.log(`Query Error: ${err}`);
-//       }
-//       if (result) {
-//         console.log(result);
-//       }
-//     });
-//   }
+import { mysqlConfig } from "..";
+import { Database } from "../db/dbConnection";
+import { Request, Response } from "express";
 
 export class Task {
-  private taskTitle;
-  private taskDesc;
-  private createdAt;
-  private deadline;
-  private createdBy;
-  private completed;
+  private connection;
+  private db;
 
-  constructor() {}
+  constructor() {
+    this.db = new Database(mysqlConfig);
+    this.connection = this.db.getConnection();
+  }
+
+  async createTask(req: Request, res: Response<any>) {
+    try {
+      const { taskTitle, taskDesc, createdAt, deadline, createdBy, completed }: any = req.body;
+      this.connection?.query("INSERT INTO task (taskTitle, taskDesc, createdAt, deadline, createdBy, completed) VALUES (?, ?, ?, ?, ?, ?)", [taskTitle, taskDesc, createdAt, deadline, createdBy, completed], (err, result) => {
+        if (err) {
+          console.log(`Query Error: ${err}`);
+        }
+        if (result) {
+          console.log(result);
+        }
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }
 }
